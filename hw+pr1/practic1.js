d0614fc114ba4f2f16675863c91b5ba18909e99b
kuchmamarta <149_149_149marta>
2021-07-14 19:33:35 +0300

    pr2

---
+++ hw+pr1/practic1.js
@@ -0,0 +1,8 @@
+let  firstName = "Olga";
+let middleName = ""
+let first_name ="Nazar"
+console.log(first_name)
+let first_name = "Igor"
+let firstName = "Anastasia"
+let firstName = "Roma"
+let firstName
